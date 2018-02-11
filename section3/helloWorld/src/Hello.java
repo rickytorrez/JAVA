3237fb3b8261f1239c611d753243331bc9936f75
rickytorrez <torrez.ricky@ymail.com>
2018-02-11 18:37:39 -0500

    helloWorld

--- section3/helloWorld/src/Hello.java
+++ section3/helloWorld/src/Hello.java
@@ -1,6 +1,23 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Udemy!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        // Challenge //
+        // Create a variable of 1000 and substract myTotal, print the result //
+
+        // int aGrand = 1000;
+        // int newTotal = aGrand - myTotal;
+
+        int newTotal = 1000 - myTotal;
+
+        System.out.println(myTotal);
+        System.out.println(newTotal);
     }
 }
 
