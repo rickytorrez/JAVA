cea7ad59cc87b955f1bac7e3aa04fdd4bd2f398a
rickytorrez <torrez.ricky@ymail.com>
2018-02-14 12:04:06 -0500

    Key Words and Expressions

---
+++ section5/KeywordsAndExpressions/src/com/ricardotorrez/Main.java
@@ -0,0 +1,35 @@
+package com.ricardotorrez;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // a mile is equal to 1.609 Kilometers
+
+        double kilometers = (100 *  1.609344);
+
+        int highScore = 50;
+
+        if(highScore == 50){
+            System.out.println("This is an expression");
+        }
+
+        // Challenge //
+
+        int score = 100;
+        if(score> 99) {
+            System.out.println("You got the highest score");
+            score = 0;
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
