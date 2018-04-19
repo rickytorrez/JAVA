33be3f30e211adca804f5192b660bcea0a39c76c
rickytorrez <torrez.ricky@ymail.com>
2018-04-19 15:10:50 -0400

    Student Data Access Layer

---
+++ End2End/studentDAL/studentdal/src/main/java/com/ericardo/studentdal/repos/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.ericardo.studentdal.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentRepository extends CrudRepository<Student, Long> {
+
+}
