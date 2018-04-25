35c8c93cff6ed3f37f5306e966b16a4f7fbf3d4d
rickytorrez <torrez.ricky@ymail.com>
2018-04-25 16:58:51 -0400

    Delete

--- End2End/location/locationweb/src/main/java/com/ericardo/location/controllers/LocationController.java
+++ End2End/location/locationweb/src/main/java/com/ericardo/location/controllers/LocationController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ericardo.location.entities.Location;
 import com.ericardo.location.service.LocationService;
@@ -38,4 +39,15 @@ public class LocationController {
 		return "displayLocations";
 	}
 
+	@RequestMapping("deleteLocation")
+	public String deleleLocation(@RequestParam("id") int id, ModelMap modelMap) {				// Retrieve parameter from ids
+//		Location location = _ls.getLocationById(id);											// Pass location once it is found by the new id
+		Location location = new Location();
+		location.setId(id);
+		_ls.deleteLocation(location);														// invoke service, retrieve and pass location
+		List<Location> locations = _ls.getAllLocations();
+		modelMap.addAttribute("locations", locations);
+		return "displayLocations";											
+	}
+	
 }
