fa5a503465dff41cba6add8104887eb868e628ac
rickytorrez <torrez.ricky@ymail.com>
2018-04-27 19:17:40 -0400

    Flight Repository

--- End2End/flightReservation/flightreservation/src/main/java/com/ericardo/flightreservation/repos/FlightRepository.java
+++ End2End/flightReservation/flightreservation/src/main/java/com/ericardo/flightreservation/repos/FlightRepository.java
@@ -1,13 +1,20 @@
 package com.ericardo.flightreservation.repos;
 
 import java.sql.Date;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ericardo.flightreservation.entities.Flight;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
 
-	void findFlights(String from, String to, Date departureDate);
+	@Query("from Flight where departureCity=:departureCity and arrivalCity-:arrivalCity and dateOfDeparture=:dateOfDeparture")
+	List<Flight> findFlights(
+			@Param("departureCity") String from, 
+			@Param("arrivalCity")String to, 
+			@Param("dateOfDeparture")Date departureDate); 
 
 }
