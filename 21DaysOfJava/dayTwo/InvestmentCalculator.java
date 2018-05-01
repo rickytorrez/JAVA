9491fae33201403737f28b9268b0d3a6f66ec88a
rickytorrez <torrez.ricky@ymail.com>
2018-05-01 16:39:37 -0400

    Dojo Go

---
+++ 21DaysOfJava/dayTwo/InvestmentCalculator.java
@@ -0,0 +1,20 @@
+public class InvestmentCalculator {
+    public static void main(String[] arguments){
+        int inv = 14000;
+        int invTwo;
+        System.out.println("How much will $" + inv + "be worth if it increased in value by 40% in it's first year...");
+
+        // Multipy inv by 40%
+        invTwo = inv * .40;
+
+        // // Add the 40% to the total
+        inv = inv + invTwo;
+
+        System.out.println("If" + inv + "lost $1500 in value the second year the total would be...");
+
+        inv -= 1500;
+
+        System.out.println(inv);
+
+    }
+}
---
+++ End2End/flightReservation/flightReserv/src/main/java/com/ericardo/flightreserv/FlightReservApplication.java
@@ -0,0 +1,12 @@
+package com.ericardo.flightreserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightReservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightReservApplication.class, args);
+	}
+}
