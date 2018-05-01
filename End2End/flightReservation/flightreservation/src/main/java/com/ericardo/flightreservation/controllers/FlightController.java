43bd7d46ec6849ed04ef4e28604be74c5893d836
rickytorrez <torrez.ricky@ymail.com>
2018-05-01 11:16:51 -0400

    Flight Display

--- End2End/flightReservation/flightreservation/src/main/java/com/ericardo/flightreservation/controllers/FlightController.java
+++ End2End/flightReservation/flightreservation/src/main/java/com/ericardo/flightreservation/controllers/FlightController.java
@@ -1,13 +1,16 @@
 package com.ericardo.flightreservation.controllers;
 
 import java.sql.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ericardo.flightreservation.entities.Flight;
 import com.ericardo.flightreservation.repos.FlightRepository;
 
 @Controller
@@ -20,9 +23,9 @@ public class FlightController {
 	public String findFlights(
 			@RequestParam("from") String from, 
 			@RequestParam("to")String to, 
-			@RequestParam("departureDate")@DateTimeFormat(pattern="MM-dd-yyyy") Date departureDate) {
-		_fR.findFlights(from,to,departureDate);
+			@RequestParam("departureDate")@DateTimeFormat(pattern="MM-dd-yyyy") Date departureDate, ModelMap modelMap) {
+		List<Flight> flights = _fR.findFlights(from,to,departureDate);
+		modelMap.addAttribute("flights", flights);
 		return "/displayFlights";
 	}
-
 }
--- End2End/flightReservation/flightreservation/src/main/java/com/ericardo/flightreservation/repos/FlightRepository.java
+++ End2End/flightReservation/flightreservation/src/main/java/com/ericardo/flightreservation/repos/FlightRepository.java
@@ -10,7 +10,8 @@ import org.springframework.data.repository.query.Param;
 import com.ericardo.flightreservation.entities.Flight;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
-	// New querys need the addition of the word value to be able to run spring
+	
+	// New query requires the addition of the word value to be able to run spring and nativeQuery at the end
 	@Query(value="from Flight where departureCity=:departureCity and arrivalCity-:arrivalCity and dateOfDeparture=:dateOfDeparture", nativeQuery=true)
 	List<Flight> findFlights(
 			@Param("departureCity") String from, 
