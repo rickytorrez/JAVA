964b3d8e236ba1eab04940f0e60c6290643c6e0c
rickytorrez <torrez.ricky@ymail.com>
2018-05-06 17:43:41 -0700

    Day Three

---
+++ 21DaysOfJava/dayThree/StringChecker.java
@@ -0,0 +1,8 @@
+package com.java21days;
+
+class StringChecker {
+    public static void main(String[] arguments) {
+        String str = "A Lannister always pays his debts";
+        System.out.println("The string is: " + str);
+    }
+}
