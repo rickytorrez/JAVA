ba7bd4d922daebffeb1100f4007185aef958fadd
rickytorrez <torrez.ricky@ymail.com>
2018-06-09 20:42:15 -0400

    REST Web Services

--- In28Mins/webServices/Spring-in-10/src/main/java/com/ericardo/springboot/basics/Springin10/BooksController.java
+++ In28Mins/webServices/Spring-in-10/src/main/java/com/ericardo/springboot/basics/Springin10/BooksController.java
@@ -1,5 +1,16 @@
 package com.ericardo.springboot.basics.Springin10;
 
-public class BooksController {
+import java.util.Arrays;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksController {
+	
+	@GetMapping("/books")
+	public List<Book> getAllBooks(){
+		return Arrays.asList(new Book(1l, "Mastering Spring 5.0", "Ranga"));
+	}
 }
